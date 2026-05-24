b5b050c10fcc4222015348f82f8ea91c624ad629
Charis Williamson <cpt533@york.ac.uk>
2026-05-24 19:17:06 +0100

    added generated probability rasters .gitignore

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -346,10 +346,12 @@ def main(args_list=None):
         gpd.points_from_xy(rand_x, rand_y), crs=topo.rio.crs
     )
 
-    df_pos = create_dataframe(topo, geo, lc, dist_fault, slope,
-                              landslide_points, landslide_label=1)
-    df_neg = create_dataframe(topo, geo, lc, dist_fault, slope,
-                              non_landslide_points, landslide_label=0)
+    df_pos = create_dataframe(
+        topo, geo, lc, dist_fault, slope, landslide_points, landslide_label=1
+    )
+    df_neg = create_dataframe(
+        topo, geo, lc, dist_fault, slope, non_landslide_points, landslide_label=0
+    )
     df = pd.concat([df_pos, df_neg], ignore_index=True).dropna()
 
     classifier = make_classifier(df.drop("ls", axis=1), df["ls"], verbose=args.verbose)
